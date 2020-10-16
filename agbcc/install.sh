be771f2f7680d7d5df8919bcb1b83002e12a90db
Alexander Nicholi <me@nicholatian.com>
2020-10-16 07:17:38 -0400

    update some minor things to make it work

--- agbcc/install.sh
+++ agbcc/install.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 set -e
 if [ "$1" != "" ]; then
-    mkdir -p $1/tools/agbcc
-    mkdir -p $1/tools/agbcc/bin
-    mkdir -p $1/tools/agbcc/include
-    mkdir -p $1/tools/agbcc/lib
-    cp agbcc $1/tools/agbcc/bin/
-    cp old_agbcc $1/tools/agbcc/bin/
-    cp agbcc_arm $1/tools/agbcc/bin/
-    cp -R libc/include $1/tools/agbcc/ #drop include, because we don't want include/include
-    cp ginclude/* $1/tools/agbcc/include/
-    cp libgcc.a $1/tools/agbcc/lib/
-    cp libc.a $1/tools/agbcc/lib/
+    mkdir -p $1
+    mkdir -p $1/bin
+    mkdir -p $1/include
+    mkdir -p $1/lib
+    cp agbcc $1/bin/
+    cp old_agbcc $1/bin/
+    cp agbcc_arm $1/bin/
+    cp -R libc/include $1/ #drop include, because we don't want include/include
+    cp ginclude/* $1/include/
+    cp libgcc.a $1/lib/
+    cp libc.a $1/lib/
 else
     echo "Usage: install.sh PATH"
 fi
